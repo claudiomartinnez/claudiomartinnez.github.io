d195308088170c4978886b7bd718914ce463791e
Claudio <claumartinnez@gmail.com>
2022-08-11 06:16:09 -0300

    login v2
    Falta redirigir

--- js/login.js
+++ js/login.js
@@ -3,11 +3,50 @@ function login (){
     let email = document.getElementById('email').value;
     let contraseña = document.getElementById('password').value;
 
-    if (email==="" || contraseña===""){
-        alert("Debe ingresar email y contraseña");
-    }else{
-        location.href='index.html';
+    // if (email ==="" && contraseña===""){
+    //     document.getElementById('email').classList.add('error');
+    //     document.getElementById('error_email').innerHTML = "Ingresar Email";
+    //     document.getElementById('password').classList.add('error');
+    //     document.getElementById('error_password').innerHTML = "Ingresar Contraseña";
+    // }
+
+    if (contraseña===""){
+        document.getElementById('password').classList.add('error');
+        document.getElementById('error_password').innerHTML = "Ingresar Contraseña";
+    } else{
+        document.getElementById('password').classList.remove('error');
+        document.getElementById('error_password').innerHTML = "";
     }
+
+    if (email===""){
+        document.getElementById('email').classList.add('error');
+        document.getElementById('error_email').innerHTML = "Ingresar Email";
+
+    } else{
+        document.getElementById('email').classList.remove('error');
+        document.getElementById('error_email').innerHTML = "";
+
+    }
+
+    if (email!=="" && contraseña!==""){
+        location.href='index.html';
+
+    }
+
+
+        
+    // } else {
+    //     location.href='index.html';
+    // }
+
+    // if (contraseña ===""){
+    //     document.getElementById('password').classList.add('error');
+    //     document.getElementById('error_password').innerHTML = "Ingresar Contraseña";
+    // } else {
+    //     location.href='index.html';
+    // }
+
+ 
 
 
 }
